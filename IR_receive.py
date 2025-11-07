60522a4ef5ffbcba1e030124d851b3518e89569c
CYF93 <muse8935@hotmail.com>
2025-11-07 17:40:03 +0800

    Add files via upload

--- IR_receive.py
+++ IR_receive.py
@@ -2,7 +2,7 @@ import pulseio
 import board
 import adafruit_irremote
 
-ir_receiver = pulseio.PulseIn(board.GP0, maxlen = 300)
+ir_receiver = pulseio.PulseIn(board.GP0, maxlen = 300) #follow the board pin connected to IR receiver's Data pin
 decoder = adafruit_irremote.GenericDecode()
 
 while True:
